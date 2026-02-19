4ee2d79d4ded9f9d9a826fdab19ca7c70231f0d4
SamuelPerezCO <sernasamuelperez@gmail.com>
2026-02-19 18:12:54 -0500

    bot works , but not on the game i dont know why

--- bot.py
+++ bot.py
@@ -1,5 +1,49 @@
+from pynput import keyboard
 from pyautogui import *
 import pyautogui
 import time
-import keyboard
 import random
+import subprocess
+
+
+running = True
+
+#import win32api , win32con (no me sirve de nada por que estoy en linux)
+
+#Tile 1 Position X: 1466 Y:1021
+#Tile 2 Position X: 1530 Y:1009
+#Tile 3 Position X: 1603 Y:1013
+#Tile 4 Position X: 1673 Y:1012
+
+def click(x, y, delay_ms=5):
+    subprocess.run(
+        ["xdotool", "mousemove", str(x), str(y), "click", "--delay", str(delay_ms), "1"],
+        check=True
+    )
+
+
+def on_press(key):
+    global running
+    try:
+        if key.char == 'q':
+            running = False
+            return False
+    except AttributeError:
+        pass
+
+listener = keyboard.Listener(on_press=on_press)
+listener.start()
+
+while running:
+
+    if pyautogui.pixel(1514 , 1100)[0] == 0:
+        click(1514 , 1100)
+    
+    if pyautogui.pixel(1593 , 1100)[0] == 0:
+        click(1593 , 1100)
+
+    if pyautogui.pixel(1670 , 1100)[0] == 0:
+        click(1670 , 1100)
+
+    if pyautogui.pixel(1752 , 1100)[0] == 0:
+        click(1752 , 1100)
